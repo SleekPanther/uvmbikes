6cfcd3300aa262463f6a4ae15742a174df0b2a7b
SleekPanther <macrobyte2@gmail.com>
2016-03-28 08:52:42 -0400

    column transitions

--- index.php
+++ index.php
@@ -8,14 +8,26 @@
         echo "<br>" . print_r($activePageArray);
         echo "<br>asd:   " . $activePageArray[$containing_folder];
         ?>
-        
+
         <div class="colContainer">
-        <div class="twoCol">LEFT<br>  
-            myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d sssssssssssssssssssssssss</div>
-        <div class="twoCol">right<br>myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d</div>
+            <div class="twoCol">LEFT<br>  
+                myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d sssssssssssssssssssssssss</div>
+            <div class="twoCol">right<br>myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d</div>
+
+        </div>
+        <div class="colContainer">
+            <div class="twoCol">LEFT<br>  
+                myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d sssssssssssssssssssssssss</div>
+            <div class="twoCol">right<br>myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d</div>
+        </div>
+        <div class="colContainer">
+            <div class="twoCol">LEFT<br>  
+                myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d sssssssssssssssssssssssss</div>
+            <div class="twoCol">right<br>myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d</div>
+        </div>
         
-    </div>&nbsp;<!-- need some content, or the box is 0px tall (all floated) -->
-</div>
+        &nbsp;<!-- need some content, or the box is 0px tall (all floated) -->
+    </div>
 </section>
 
 <?php include($upFolderPlaceholder . "non-pages/php-include/footer.php"); ?>
