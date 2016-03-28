cd85276868a60f47ca78b855a227c48b0b77aecc
SleekPanther <macrobyte2@gmail.com>
2016-03-28 07:54:22 -0400

    columns work

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
-<?php include("non-pages/php-include/top.php");  ?>
+<?php include("non-pages/php-include/top.php"); ?>
 
+<section>
+    <div class="widthContainer">
         <h1>Noah Patullo CS142</h1>
-        <?php echo $upFolderPlaceholder;
-        echo "<br>". print_r($activePageArray);
-       echo "<br>asd:   ". $activePageArray[$containing_folder];
-            ?>
-    </body>
+        <?php
+        echo $upFolderPlaceholder;
+        echo "<br>" . print_r($activePageArray);
+        echo "<br>asd:   " . $activePageArray[$containing_folder];
+        ?>
+        
+        <div class="colContainer">
+        <div class="twoCol">LEFT<br>  
+            myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d sssssssssssssssssssssssss</div>
+        <div class="twoCol">right<br>myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d</div>
+        &nbsp;<!-- need some content, or the box is 0px tall (all floated) -->
+    </div>&nbsp;<!-- need some content, or the box is 0px tall (all floated) -->
+</div>
+</section>
+
+<?php include($upFolderPlaceholder . "non-pages/php-include/footer.php"); ?>
+</body>
 </html>
---
+++ non-pages/php-include/footer.php
@@ -0,0 +1,12 @@
+<!-- start footer section-->
+<footer>
+    <div class="widthContainer">
+        <div class="colContainer">
+        <div class="twoCol">left<br>  
+        myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d</div>
+        <div class="twoCol">right<br>myasdf;lkas gd;lkas dg;ndsf;jg a;lkngf ;daf;lknad fg;lk adsjfngksdfjbg skhb dsfghbsdfgkjhbs dfkjhbsdfgkjhbsdfgkjhb sdf hlkdsfgk jkhbhgb gfhfg fghbgf d</div>
+        &nbsp;<!-- need some content, or the box is 0px tall (all floated) -->
+    </div>
+    </div>
+</footer>
+<!-- end footer section-->
