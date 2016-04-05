9b06b7477ad5d395361c7018938ce35071fd72c8
SleekPanther <macrobyte2@gmail.com>
2016-04-05 09:35:16 -0400

    facebook page embed (still need with fixes)

--- contact/index.php
+++ contact/index.php
@@ -15,8 +15,22 @@
                     <p>Contact:</p>
                         <p class="contactLink">Noah Patullo <a href="mailto:macrobyte2+bikes@gmail.com">Macrobyte2+bikes@gmail.com</a></p>
                         
-                    <div>
-                        facebook
+                    <div class='subSec'>
+                        <div id="fb-root"></div>
+                        <script>(function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) return;
+                            js = d.createElement(s); js.id = id;
+                            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+                            fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));
+                        </script>
+                        
+                        <div class='fbPageEmbedContainer'>
+                            <div class="fb-page" data-href="https://www.facebook.com/UVMBIKES/" data-tabs="timeline" data-width="500px" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                                <div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/UVMBIKES/"><a href="https://www.facebook.com/UVMBIKES/">UVM Bikes</a></blockquote></div>
+                            </div>
+                        </div>
                     </div>
                 </article>
             </div>
