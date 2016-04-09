ef748781971833d9336cb09d47d4885dd7f442b0
SleekPanther <macrobyte2@gmail.com>
2016-04-09 09:32:27 -0400

    hours table highlight works

--- hours/index.php
+++ hours/index.php
@@ -13,12 +13,21 @@
             ?>
             <table>
             <?php
-            echo print_r($currentDayHoursTableActiveArray);
+            $daysOfWeekArray = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");   //array to be used as keys for the 2nd array
+            $currentDayHoursTableActiveArray = array();     //initialize a blank array to hold "currentDayHoursTable", a class highlighting the current day
+            $currentDayHoursTableActiveArray = array_fill_keys($daysOfWeekArray, '');        //need to fill the array with something so it exists. Start with blank, but works with any character. Just as long as the array has a key, it will be have a space to be overwritten
+            //uses $today, a string variable holding the current day, assigned in header.php with php getdate()
+            for($i = 0; $i < count($daysOfWeekArray) ; $i++){      //loop through the $daysOfWeekArray array 
+                if($today == $daysOfWeekArray[$i]){   //if the current day matched the kay in the 2nd array
+                    $currentDayHoursTableActiveArray[$today]= "currentDayHoursTable";     //print "currentDayHoursTable" in the $currentDayHoursTableActiveArray, at the index of $today. It's associative, so $currentDayHoursTableActiveArray must be accessed via a key, in this case it's the $today
+                    break;      //if it finds the current day, break out of the loop, there's no point in continuing.
+                }
+            }
+            
                 echo "\t<tr>\n";        //start table row for headers  (Sunday, Monday ...
                 for($i = 0; $i < 7; $i++){       //go from 0 to 7 (just in canse there's any extra text in the row, only bother pringing the 1st 7 days)
                     
-                    echo "\t\t\t<th class=\"" . $currentDayHoursTableActiveArray[$hoursFileContents[0][$i]] . "\">",
-                    //echo "\t\t\t<th class=\"". $currentDayHoursTableActiveArray['Saturday'] .">",
+                    echo "\t\t\t<th class=\"" . $currentDayHoursTableActiveArray[$hoursFileContents[0][$i]] . "\">",    //print class='currentDayHoursTable' for the current day. 0th row, $i'th element in the row. This is the header row, so the contents match
                             $hoursFileContents[0][$i],      //print the 1st row, & all elements in the row
                         "</th>\n";
                 }
@@ -26,7 +35,7 @@
                 
                 echo "\t\t<tr>\n";          //print the actual hours using a td instead of th
                 for($i = 0; $i < 7; $i++){
-                    echo "\t\t\t<td class=\"" . $currentDayHoursTableActiveArray[$hoursFileContents[0][$i]] . "\">",
+                    echo "\t\t\t<td class=\"" . $currentDayHoursTableActiveArray[$hoursFileContents[0][$i]] . "\">",    //print class='currentDayHoursTable' for the current day. This is the 2nd row down, but it still need to be compared based on the value in the header row: 0th row, $i'th element in the row
                             $hoursFileContents[1][$i],
                         "</td>\n";
                 }
