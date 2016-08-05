93255acd7628eb5038fd6401caef9d24c9ca80a0
SleekPanther <macrobyte2@gmail.com>
2016-08-05 10:59:15 -0400

    replaced 2 echos with . for concatenation

--- non-pages/php-include/nav.php
+++ non-pages/php-include/nav.php
@@ -5,14 +5,14 @@
         <input type="checkbox" id="drop" />
         <ul class="menu">
             <!-- IMPORTANT $containing_folder == $ROOT_DIRECTORY is different from all other links since it has a special parent folder. (or we don't know what the server's parent folder will be. So this must be the ver base case for comparison. -->
-            <li><a <?php echo 'href="'.$upFolderPlaceholder.'index.php"'; echo ' class="'.$activePageArrayTop[$ROOT_DIRECTORY].'"'; ?>>Home</a></li>
+            <li><a <?php echo 'href="'.$upFolderPlaceholder.'index.php"' . ' class="'.$activePageArrayTop[$ROOT_DIRECTORY].'"'; ?>>Home</a></li>
             <li>
                 <!-- First Tier Drop Down -->
                 <label for="drop-2" class="toggle">Portfolio +</label>
                 <a href="#">Portfolio</a>
                 <input type="checkbox" id="drop-2"/>
                 <ul>
-                    <li><a <?php echo 'href="'.$upFolderPlaceholder.'portfolio/portfolio_1/index.php"' ; ?>>Portfolio 1</a></li>
+                    <li><a <?php echo 'href="'.$upFolderPlaceholder.'portfolio/portfolio_1/index.php"' . ' class="'.$activePageArrayDropDown1['portfolio_1'].'"'; ?>>Portfolio 1</a></li>
                     <li><a href="sub1">Portfolio 2</a></li>
                     <li>
                         <!-- Second Tier Drop Down -->
@@ -27,15 +27,21 @@
                     </li>
                 </ul>
             </li>
-            <li><a <?php echo 'href="'.$upFolderPlaceholder.'prices-services/index.php"'; echo ' class="'.$activePageArrayTop['prices-services'].'"'; ?>>Prices / Services</a></li>
-            <li><a <?php echo 'href="'.$upFolderPlaceholder.'hours/index.php"'; echo ' class="'.$activePageArrayTop['hours'].'"'; ?>>Hours</a></li>
-            <li><a <?php echo 'href="'.$upFolderPlaceholder.'classes-events/index.php"'; echo ' class="'.$activePageArrayTop['classes-events'].'"'; ?>>Classes / Events</a></li>
-            <li><a <?php echo 'href="'.$upFolderPlaceholder.'contact/index.php"'; echo ' class="'.$activePageArrayTop['contact'].'"'; ?>>Contact</a></li>
-            <li><a <?php echo 'href="'.$upFolderPlaceholder.'about/index.php"'; echo ' class="'.$activePageArrayTop['about'].'"'; ?>>About</a></li>
+            <li><a <?php echo 'href="'.$upFolderPlaceholder.'prices-services/index.php"' . ' class="'.$activePageArrayTop['prices-services'].'"'; ?>>Prices / Services</a></li>
+            <li><a <?php echo 'href="'.$upFolderPlaceholder.'hours/index.php"' . ' class="'.$activePageArrayTop['hours'].'"'; ?>>Hours</a></li>
+            <li><a <?php echo 'href="'.$upFolderPlaceholder.'classes-events/index.php"' . ' class="'.$activePageArrayTop['classes-events'].'"'; ?>>Classes / Events</a></li>
+            <li><a <?php echo 'href="'.$upFolderPlaceholder.'contact/index.php"' . ' class="'.$activePageArrayTop['contact'].'"'; ?>>Contact</a></li>
+            <li><a <?php echo 'href="'.$upFolderPlaceholder.'about/index.php"' . ' class="'.$activePageArrayTop['about'].'"'; ?>>About</a></li>
         </ul>
 </nav>
-<p>ha<br><br>
+<p>top level pages<br>
     <?php print_r($activePageArrayTop) ?>
-    <br><br>end</p>
+    <br><br>drop1
+    <?php print_r($activePageArrayDropDown1) ?>
+    <br><br>drop 2
+    <?php print_r($activePageArrayDropDown2) ?>
+    
+    <br><br>end
+</p>
 <!-- end nav.php -->
 
